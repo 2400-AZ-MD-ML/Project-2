2bbcddd0b00cea02cee0fcbfd08d31eb07d66344
mmlado-cpp <98682021+mmlado-cpp@users.noreply.github.com>
2022-03-17 11:38:18 -0700

    ArrayStack tests added
    still need to fix evaulatePostfix method in ArrayStack.java

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,5 +1,22 @@
 package Project2;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 public class ArrayStackTest {
-    
+    @Test
+    public void evaluatePostfixTest1(){
+        ArrayStack<String> test = new ArrayStack<String>();
+        assertEquals("-10", test.evaluatePostfix("234-/5*"));
+    }
+    @Test
+    public void evaluatePostfixTest2(){
+        ArrayStack<String> test = new ArrayStack<String>();
+        assertEquals("19", test.evaluatePostfix("4 52* +5 +"));
+    }
+    @Test
+    public void evaluatePostfixTestError(){
+        ArrayStack<String> test = new ArrayStack<String>();
+        Assertions.assertThrows(IllegalStateException.class, ()-> {test.evaluatePostfix(" ");}, "Empty String");
+    }
 }
